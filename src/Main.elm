1d7c136b208d236fb581a41193335c8a020d1c17
Florian Fesseler <ffesseler@kapit.fr>
2016-10-17 17:39:32 +0200

    fetch profile when input changes

--- src/Main.elm
+++ src/Main.elm
@@ -2,31 +2,80 @@ module Main exposing (..)
 
 import Html exposing (text, Html, nav, i, a, li, div, input, form, b, p, h5)
 import Html.Attributes exposing (class, attribute, href, type', placeholder, id, name)
+import Html.Events exposing (onInput)
 import Html.App as App
+import Http
+import Json.Decode as Decode exposing ((:=))
+import Task
 
 
 main =
-    App.program { init = init "", update = update, view = view, subscriptions = \_ -> Sub.none }
+    App.program { init = init "" (Credentials "" ""), update = update, view = view, subscriptions = \_ -> Sub.none }
 
 
 type alias Model =
     { githubName : String
+    , credentials : Credentials
+    , profile : Maybe GithubProfile
+    , error : Maybe Http.Error
+    }
+
+
+type alias Credentials =
+    { clientId : String
+    , clientSecret : String
+    }
+
+
+type alias GithubProfile =
+    { name : String
+    , repos_url : String
+    }
+
+
+type alias Repository =
+    { name : String
     }
 
 
 type Msg
-    = NoOp
+    = SearchUser String
+    | FetchUserDone (GithubProfile)
+    | FetchUserFail Http.Error
 
 
-init githubName =
-    ( Model githubName, Cmd.none )
+init githubName credentials =
+    ( Model githubName credentials Nothing Nothing, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        SearchUser githubName ->
+            ( model, fetchGithubUser githubName model.credentials )
+
+        FetchUserDone profile ->
+            ( { model | profile = Just profile }, Cmd.none )
+
+        FetchUserFail error ->
+            Debug.log "fail" ( { model | error = Just error }, Cmd.none )
+
+
+fetchGithubUser githubName credentials =
+    Http.get profileDecoder (fetchGithubUserUrl githubName credentials)
+        |> Task.perform FetchUserFail FetchUserDone
+
+
+fetchGithubUserUrl : String -> Credentials -> String
+fetchGithubUserUrl name credentials =
+    "http://api.github.com/users/" ++ name ++ "?client_id=" ++ credentials.clientId ++ "&client_secret=" ++ credentials.clientSecret
+
+
+profileDecoder : Decode.Decoder (GithubProfile)
+profileDecoder =
+    Decode.object2 GithubProfile
+        ("name" := Decode.string)
+        ("repos_url" := Decode.string)
 
 
 view : Model -> Html Msg
@@ -38,18 +87,25 @@ view model =
 
 
 profile model =
-    div [ class "m-x-auto" ]
-        [ div [ class "starter-template text-xs-center" ]
-            [ h5 []
-                [ text "Search any user" ]
-            , p [ class "lead" ]
-                [ text "Please enter a "
-                , b []
-                    [ text "username" ]
-                , text "."
+    case model.profile of
+        Nothing ->
+            div [ class "m-x-auto" ]
+                [ div [ class "starter-template text-xs-center" ]
+                    [ h5 []
+                        [ text "Search any user" ]
+                    , p [ class "lead" ]
+                        [ text "Please enter a "
+                        , b []
+                            [ text "username" ]
+                        , text "."
+                        ]
+                    ]
                 ]
-            ]
-        ]
+
+        Just profile ->
+            div []
+                [ h5 [] [ text profile.name ]
+                ]
 
 
 searchform model =
@@ -63,7 +119,7 @@ searchform model =
                 [ form [ id "searchForm" ]
                     [ div [ class "form-group" ]
                         [ div [ class "input-group" ]
-                            [ input [ class "form-control", name "username", placeholder "Enter A Github Username...", type' "text" ]
+                            [ input [ class "form-control", name "username", placeholder "Enter A Github Username...", type' "text", onInput SearchUser ]
                                 []
                             , div [ class "input-group-addon" ]
                                 [ i [ attribute "aria-hidden" "true", class "fa fa-keyboard-o" ]
