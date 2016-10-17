6adb12ee368093b07242ecd3a6e052fdbfd9f390
Florian Fesseler <ffesseler@kapit.fr>
2016-10-17 16:10:46 +0200

    add elm architecture

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,33 @@
 module Main exposing (..)
 
 import Html exposing (text, Html)
+import Html.App as App
 
 
-main : Html msg
 main =
-    text "Hello World!"
+    App.program { init = init "", update = update, view = view, subscriptions = \_ -> Sub.none }
+
+
+type alias Model =
+    { githubName : String
+    }
+
+
+type Msg
+    = NoOp
+
+
+init githubName =
+    ( Model githubName, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    text "Hello World"
